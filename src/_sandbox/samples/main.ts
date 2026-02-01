229b4b513957a1fafddb6f00b77947cfef4e7e78
SERDUN <dmitriiserdun@gmail.com>
2026-02-01 11:22:40 +0200

    feat: implement recursive module registration via imports

--- src/_sandbox/samples/main.ts
+++ src/_sandbox/samples/main.ts
@@ -4,24 +4,29 @@ import { Module } from "../../common/decorators/module.decorator.js";
 import { NestFactory } from "../../common/utils/nest.factory.js";
 
 @Injectable()
-class LoggerService {
-  log(message: string) {
-    console.log(`[Logger]: ${message}`);
-  }
+class UsersService {
+  findAll() { return ['Alice', 'Bob']; }
 }
+
 
 @Injectable()
 class AppService {
-  constructor(private logger: LoggerService) {}
+  constructor(private usersService: UsersService) {}
 
-  getHello() {
-    this.logger.log("Returning hello message");
-    return "Hello form Mini-Nest!";
+  printUsers() {
+    console.log('Users:', this.usersService.findAll());
   }
 }
 
+
 @Module({
-  providers: [AppService, LoggerService],
+  providers: [UsersService],
+})
+class UsersModule {}
+
+@Module({
+  imports: [UsersModule],
+  providers: [AppService],
 })
 class AppModule {}
 
@@ -32,6 +37,5 @@ export const runMain = async () => {
   const app = await NestFactory.create(AppModule);
 
   const appService = app.get<AppService>(AppService);
-
-  console.log(appService.getHello());
+  appService.printUsers();
 }
--- src/common/utils/nest.factory.ts
+++ src/common/utils/nest.factory.ts
@@ -1,19 +1,25 @@
 import { ServiceLocator } from "./service-locator.js";
 import { NestApplication } from "./nest.application.js";
-import { MODULE_CONTROLLERS, MODULE_IMPORTS, MODULE_PROVIDERS } from "../types/metadata.keys.js";
+import { MODULE_IMPORTS, MODULE_PROVIDERS } from "../types/metadata.keys.js";
 
 export class NestFactory {
     static async create(rootModule: any): Promise<NestApplication> {
       const serviceLocator = new ServiceLocator();
 
-      const imports = Reflect.getOwnMetadata(MODULE_IMPORTS, rootModule) || [];
-      const providers = Reflect.getOwnMetadata(MODULE_PROVIDERS, rootModule) || [];
-      const controllers = Reflect.getOwnMetadata(MODULE_CONTROLLERS, rootModule) || [];
-
-      providers.forEach((provider: any) => {
-        serviceLocator.register(provider);
-      })
+      await this.bind(rootModule, serviceLocator)
 
       return new NestApplication(serviceLocator);
+    }
+
+    static async bind(module: any,service:ServiceLocator) {
+      const providers = Reflect.getOwnMetadata(MODULE_PROVIDERS, module) || [];
+      providers.forEach((provider: any) => {
+        service.register(provider);
+      })
+
+      const imports = Reflect.getOwnMetadata(MODULE_IMPORTS, module) || [];
+      imports.forEach((imp: any) => {
+        this.bind(imp,service);
+      })
     }
 }
